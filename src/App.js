22ef1753c5f82c32d9aa2e74a0744e085cb06892
emiliazm <98142626+emiliazm@users.noreply.github.com>
2022-06-13 22:16:01 +0200

    Add nav.js and Links on App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+import Nav from './components/Nav';
+import Rockets from './pages/Rockets';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="*" element={<Navigate to="/" />} />
+        {/* <Route path="/Missions" element={<Missions />} />
+        <Route path="/myProfile" element={<miPorfile />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
