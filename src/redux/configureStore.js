5451522b26446528f0becdc017b2a43c64920d64
ONJoseph <on.josephinfo@gmail.com>
2022-06-14 20:48:52 +0100

    fix mission link

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import rocketsReducer from './rockets.js/rockets';
+import missionsReducer from './missions/missionsReducer';
 
 const store = configureStore({
   reducer: {
     rockets: rocketsReducer,
+    missions: missionsReducer,
   },
   middleware: [thunk],
 });
