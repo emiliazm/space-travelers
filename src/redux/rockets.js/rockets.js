2e1e28888c8b11996890369c5f0450c99932e1a9
emiliazm <98142626+emiliazm@users.noreply.github.com>
2022-06-14 18:04:53 +0200

    Implement rocket booking, cancelation and badges

--- src/redux/rockets.js/rockets.js
+++ src/redux/rockets.js/rockets.js
@@ -2,6 +2,7 @@ import getRocketApi from '../../Api/rocketsApi';
 
 const GET_ROCKETS = 'spacetravelers/rockets/GET_ROCKETS';
 const RESERVE_ROCKETS = 'spacetravelers/rockets/RESREVE_ROCKETS';
+const CANCEL_RESERVE = 'spacetravelers/rockets/CANCEL_RESERVE';
 
 const initialState = [];
 
@@ -9,7 +10,7 @@ export const getRockets = () => async (dispatch) => {
   const data = await getRocketApi();
   const rockets = data.map((rocket) => {
     const {
-      id, rocket_name: rocketName, description, flickr_images: flickrImages,
+      id, rocket_name: rocketName, description, flickr_images: flickrImages, reserved,
     } = rocket;
 
     return {
@@ -17,34 +18,32 @@ export const getRockets = () => async (dispatch) => {
       rocketName,
       description,
       flickrImages: flickrImages[0] || '',
+      reserved,
     };
   });
   dispatch({ type: GET_ROCKETS, rockets });
 };
 
-export function reserveRockets(id) {
-  return () => ({
-    type: RESERVE_ROCKETS,
-    id,
-  });
-}
+export const cancelRockets = (id) => ({ type: CANCEL_RESERVE, id });
+export const reserveRockets = (id) => ({ type: RESERVE_ROCKETS, id });
+
+const changeReserved = (state, action, reserved) => [...state].map((rocket) => {
+  if (action.id === rocket.id) {
+    return { ...rocket, reserved };
+  }
+  return rocket;
+});
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case CANCEL_RESERVE:
+      return changeReserved(state, action, false);
+
     case GET_ROCKETS:
       return action.rockets;
 
-      // case RESERVE_ROCKETS:
-      //   const newState = state.map((rocket) => {
-      //     if (action.id === rocket.id) {
-      //       return {
-      //         ...rocket,
-      //         reserved: !rocket.reserved,
-      //       };
-      //     }
-      //     return rocket;
-      //   });
-      //   return newState;
+    case RESERVE_ROCKETS:
+      return changeReserved(state, action, true);
 
     default:
       return state;
