fb78c47fa144d266f8ca9521bed293b4ab5df8ad
ONJoseph <on.josephinfo@gmail.com>
2022-06-14 20:04:57 +0100

    add missions fetch and reducers API

---
+++ src/components/Missions/Missions.js
@@ -0,0 +1,41 @@
+import { React, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import Mission from './Mission';
+import missionsFetch from '../../redux/missions/missionsFetch';
+import './missions.css';
+
+const Missions = () => {
+  const missions = useSelector((state) => state.missionsReducer);
+  const dispatch = useDispatch();
+
+  useEffect(() => { dispatch(missionsFetch()); }, []);
+
+  return (
+    <div className="container">
+      <table className="table">
+        <tbody>
+          <tr>
+            <th className="head">Missions</th>
+            <th className="head">Description</th>
+            <th className="head">Status</th>
+            <th className="head">Action</th>
+          </tr>
+          {missions.map(
+            (mission) => (
+              <Mission
+                key={uuidv4()}
+                mission_name={mission.mission_name}
+                description={mission.description}
+                mission_id={mission.mission_id}
+                reserved={mission.reserved}
+              />
+            ),
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Missions;
